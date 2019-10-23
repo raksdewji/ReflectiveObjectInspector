578497e31c4e82def05b9a7965acb9669dd36a58
raksdewji <rakheemdewji@hotmail.com>
2019-10-23 17:40:13 -0600

    re-ran driver to fix script files

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -8,7 +8,7 @@ import java.io.*;
 public class Driver {
 
     public static void main(String[] args) throws Exception {
-        boolean rec = true;
+        boolean rec = false;
         if (args.length == 2) {
             rec = Boolean.parseBoolean(args[1]);
         }
